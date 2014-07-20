a3921471c60734b5fe59dd240f13a4925012ce3a
Weipin Xia <weipin@me.com>
2014-07-20 22:06:03 +0800

    First commit

---
+++ URITemplateTouch/URITemplateTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  URITemplateTouch
+//
+//  Created by Weipin Xia on 7/20/14.
+//  Copyright (c) 2014 Weipin Xia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ source/URITemplate.swift
@@ -0,0 +1,10 @@
+//
+//  URITemplate.swift
+//  URITemplateTouch
+//
+//  Created by Weipin Xia on 7/20/14.
+//  Copyright (c) 2014 Weipin Xia. All rights reserved.
+//
+
+import Foundation
+
