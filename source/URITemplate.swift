843973aa5b5e31d3f76631606f3c49da1802df75
Weipin Xia <weipin@me.com>
2014-07-22 12:43:46 +0800

    Add ScanningLiterals

--- source/URITemplate.swift
+++ source/URITemplate.swift
@@ -14,21 +14,13 @@ import Foundation
 //    CFStringBuiltInEncodings.UTF8.toRaw())
 
 enum URITemplateError {
-
+    case MalformedPctEncodedInLiterals
 }
 
 class URITemplate {
     enum State {
-        case Idle
-        case LiteralsStart
-        case LiteralsIn
-        case LiteralsInPctEncodedStart
-        case LiteralsInPctEncodedIn
-        case LiteralsInPctEncodedEnd
-        case LiteralsEnd
-        case ExpressionStart
-        case ExpressionIn
-        case ExpressionEnd
+        case ScanningLiterals
+        case ScanningExpressions
     }
 
     enum BehaviorAllow {
@@ -58,32 +50,101 @@ class URITemplate {
                 "&"  : Behavior(first: "&", sep: "&", named: true,  ifemp: "=", allow: .U),
                 "#"  : Behavior(first: "#", sep: ",", named: false, ifemp: "",  allow: .UR),
             ]
-            static let HEXDIG = NSCharacterSet(charactersInString: "0123456789abcdefABCDEF")
-            static let RESERVED = NSCharacterSet(charactersInString: ":/?#[]@!$&'()*+,;=")
-            static let UNRESERVED = NSCharacterSet(charactersInString: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~") // 66
+            static let HEXDIG = "0123456789abcdefABCDEF"
+            static let RESERVED = ":/?#[]@!$&'()*+,;="
+            static let UNRESERVED = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~" // 66
         }
 
-        var state: State = .Idle
-        var result = ""
-        var error: URITemplateError?
+        let BehaviorTable = ClassVariable.BehaviorTable
+        let HEXDIG = ClassVariable.HEXDIG
+        let RESERVED = ClassVariable.RESERVED
+        let UNRESERVED = ClassVariable.UNRESERVED
 
-        for (c, index) in enumerate(template) {
-            switch state {
-            case .Idle:
-                println("")
-            case .LiteralsStart:
-                println("")
-            case .LiteralsIn:
-                println("")
-            case .ExpressionStart:
-                println("")
-            case .ExpressionIn:
-                println("")
-            case .ExpressionEnd:
-                println("")
+
+        // Pct-encoded isn't taken into account
+        func appendLiteralCharacter(character: Character, toString: String) -> String {
+            if find(RESERVED, character) || find(UNRESERVED, character) {
+                var str = toString + character
+                return str
             }
+
+            var str = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                            String(character).bridgeToObjectiveC(), nil, nil,
+                            CFStringBuiltInEncodings.UTF8.toRaw())
+            return str
         }
 
-        return ""
-    }
-}
+        func appendLiteralString(string: String, toString: String) -> String {
+            var result = toString
+            for c in string {
+                result = appendLiteralCharacter(c, result)
+            }
+            return result
+        }
+
+        var state: State = .ScanningLiterals
+        var result = ""
+        var pctEncoded = ""
+        var errors = Dictionary<Int, URITemplateError>()
+
+        for (index, c) in enumerate(template) {
+            switch state {
+            case .ScanningLiterals:
+                if c == "(" {
+                    state = .ScanningExpressions
+
+                } else if (countElements(pctEncoded) > 0) {
+                    switch countElements(pctEncoded) {
+                    case 1:
+                        if find(HEXDIG, c) {
+                            pctEncoded += c
+                        } else {
+                            errors[index] = URITemplateError.MalformedPctEncodedInLiterals
+                            result = appendLiteralString(pctEncoded, result)
+                            result = appendLiteralCharacter(c, result)
+                            state = .ScanningLiterals
+                            pctEncoded = ""
+                        }
+
+                    case 2:
+                        if find(HEXDIG, c) {
+                            pctEncoded += c
+                            result += pctEncoded
+                            state = .ScanningLiterals
+                            pctEncoded = ""
+
+                        } else {
+                            errors[index] = URITemplateError.MalformedPctEncodedInLiterals
+                            result = appendLiteralString(pctEncoded, result)
+                            result = appendLiteralCharacter(c, result)
+                            state = .ScanningLiterals
+                            pctEncoded = ""
+                        }
+
+                    default:
+                        assert(false)
+                    }
+
+                } else if c == "%" {
+                    pctEncoded += c
+                    state = .ScanningLiterals
+
+                } else if find(UNRESERVED, c) || find(RESERVED, c) {
+                    result += c
+
+                } else {
+
+                }
+
+            case .ScanningExpressions:
+                println("")
+
+            default:
+                assert(false)
+            } // switch
+        }// for
+
+        return result
+    } // process
+
+} // URITemplate
